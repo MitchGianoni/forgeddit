bdc901b9596ec5bea0f56a59d24788201884a800
MitchGianoni <mitchgianoni@yahoo.com>
2016-03-31 16:12:06 -0400

    Added user model and migration, current user method in application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+  	user_id = sessuin[:user_id]
+  	if user_id
+  		User.find_by(id: user_id
+  	end
+  end)
 end
