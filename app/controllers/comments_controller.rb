f79be13dbe37d345c8fdc008890289acb8cc0996
MitchGianoni <mitchgianoni@yahoo.com>
2016-04-05 15:34:45 -0400

    Added user association functionality and destroy method.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,20 @@
 class CommentsController < ApplicationController
+	before_action :authenticate!
+
 	def create
 		@post = Post.find(params["id"])
-		@post.comments.create(text: params["text"])
-		redirect_to posts_path(@post)
+		@post.comments.create(text: params["text"], user_id: current_user.id)
+		redirect_to posts_show_path(@post)
+	end
+
+	def destroy
+		c = Comment.find(params["id"])
+		@post = c.post_id
+		if current_user.id == c.user_id
+			c.destroy
+		else
+			flash[:notice] = "You can't delete other users' posts!"
+		end
+		redirect_to posts_show_path(@post)
 	end
 end
